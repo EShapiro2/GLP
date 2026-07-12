a554ae849fb274faa03d72db5f408cc207fcbcc4
EShapiro2 <e.shapiro2@lse.ac.uk>
2026-07-12 18:37:37 +0300

    GrassApp Duo: generated plugin registrant and Xcode project updates for the grassroots_networking pods

--- glp_multiagent/macos/Flutter/GeneratedPluginRegistrant.swift
+++ glp_multiagent/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import file_selector_macos
+import flutter_local_notifications
+import flutter_secure_storage_darwin
+import shared_preferences_foundation
+import sodium_libs
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SodiumLibsPlugin.register(with: registry.registrar(forPlugin: "SodiumLibsPlugin"))
 }
